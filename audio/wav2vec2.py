eff3d89c5cbc126297b92bd75a86aa3e359b5d8c
SeongMin2 <soobark97@naver.com>
2022-03-17 19:06:16 +0900

    Add audio

---
+++ audio/wav2vec2.py
@@ -0,0 +1,19 @@
+# https://huggingface.co/facebook/wav2vec2-large-lv60
+from transformers import AutoProcessor, AutoModelForPreTraining
+import librosa
+import torch
+
+processor = AutoProcessor.from_pretrained("facebook/wav2vec2-large-lv60")
+
+model = AutoModelForPreTraining.from_pretrained("facebook/wav2vec2-large-lv60")
+print(model)
+# 해당 모델이 16K sampling rate로 학습시켰기 때문
+
+sr = 16000
+vector, _sr = librosa.load('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav',sr=sr)
+
+vector = torch.tensor(vector)
+vector = torch.unsqueeze(vector,0)
+result = model(vector)
+
+pass
---
+++ audio/wave-preprocessing.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import tqdm
+import librosa
+
+# WAVE_PATH = '../IEMOCAP_full_release/Session{}/sentences/wav'
+'''
+labels_df = pd.read_csv('../extraction/df_iemocap_sess1.csv')
+
+for sess in [1]:
+    wav_file_path = '../IEMOCAP_full_release/Session{}/sentences/wav'.format(sess)
+    audio_list = os.listdir(wav_file_path)
+
+    for audio in audio_list:
+        wav_files = os.listdir(wav_file_path + audio)
+
+        for wav_file in wav_files:
+            librosa.load(wav_file_path+audio+'/'+wav_file)
+'''
+sr = 44100
+vector, _sr = librosa.load('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav',sr=sr)
