12d3ab70844d7723f79db7054b5277295ca9822d
SeongMin2 <soobark97@naver.com>
2022-03-18 09:55:00 +0900

    Nothing

--- audio/wave-preprocessing.py
+++ audio/wave-preprocessing.py
@@ -3,11 +3,15 @@ import sys
 import numpy as np
 import pandas as pd
 import tqdm
+import torch
 import librosa
 
 # WAVE_PATH = '../IEMOCAP_full_release/Session{}/sentences/wav'
-'''
+
+
 labels_df = pd.read_csv('../extraction/df_iemocap_sess1.csv')
+SR = 16000
+audio_list = []
 
 for sess in [1]:
     wav_file_path = '../IEMOCAP_full_release/Session{}/sentences/wav'.format(sess)
@@ -17,7 +21,11 @@ for sess in [1]:
         wav_files = os.listdir(wav_file_path + audio)
 
         for wav_file in wav_files:
-            librosa.load(wav_file_path+audio+'/'+wav_file)
-'''
+            vector, _sr = librosa.load(wav_file_path+audio + '/' + wav_file, sr = SR)
+            audio_list.append(vector)
+
+
+
+
 sr = 44100
 vector, _sr = librosa.load('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav',sr=sr)
---
+++ nothing.py
@@ -0,0 +1,12 @@
+import torch
+
+a= []
+a.append([1,2,3])
+a.append([3,4,5,6])
+#b = torch.tensor(a)
+print(a)
+#print(b)
+
+d = dict()
+d['a'](1)
+print(d)
