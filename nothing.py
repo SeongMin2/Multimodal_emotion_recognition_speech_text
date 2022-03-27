839c11e592db8b3c53732a63ec361deb73e7dd01
SeongMin2 <soobark97@naver.com>
2022-03-27 20:34:43 +0900

    Remove non useful codes

--- nothing.py
+++ nothing.py
@@ -79,3 +79,40 @@ def train(model, optimizer, train_iter):
 best_val_loss = None
 for e in range(1, EPOCHS+1):
     train(model, optimizer, train_iter)
+
+
+# wav2vec text
+'''
+# https://huggingface.co/facebook/wav2vec2-large-lv60
+from transformers import AutoProcessor, AutoModelForPreTraining # 여기서 왜 AutoProcessor 머가 문제야
+import librosa
+import torch
+
+processor = AutoProcessor.from_pretrained("facebook/wav2vec2-large-lv60")
+
+model = AutoModelForPreTraining.from_pretrained("facebook/wav2vec2-large-lv60")
+print(model)
+# 해당 모델이 16K sampling rate로 학습시켰기 때문
+
+sr = 16000
+vector, _sr = librosa.load('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav',sr=sr)
+
+vector = torch.tensor(vector)
+vector = torch.unsqueeze(vector,0)
+result = model(vector)
+
+pass
+'''
+
+# soundfild test
+'''
+import soundfile as sf
+import librosa
+sr = 16000
+data, samplerate = sf.read('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav')
+# default가 16000인듯 왜 근데 공홈에 뚜렷히 안나와 있지..
+
+vector, _sr = librosa.load('../IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav',sr=sr)
+
+pass
+'''
